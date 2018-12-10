1af7c58281e3ea45fbeb432fd20893ac7ec7d77f
professorsfault <45768946+professorsfault@users.noreply.github.com>
2018-12-10 19:35:15 +0100

    An: Frau Hopper
    Antwort zu den Softwaretests

--- hello_world.c
+++ hello_world.c
@@ -1,3 +1,15 @@
+Liebe Frau Hopper,
+
+ich denke Sie haben recht! In meinem
+Programm ist ein "bug". Gut, dass Sie
+mir rechtzeitig bescheid gegeben haben,
+ansonsten würde dies bei den Nutzern zu
+großen Fehlern führen.
+  
+Mit freundlichen Grüßen,
+
+Professor M. Y. Fault
+
 #include <stdio.h>
 
 int main(int argc, char *argv[])
